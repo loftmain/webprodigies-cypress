86060b5918d2c400e855d55ee207d944d73dcc67
jinjaelee <jjlee009@newsystech.co.kr>
2025-01-23 18:10:50 +0900

    card 만들기까지

---
+++ src/components/landing-page/title-section.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+interface TitleSectionProps {
+  title: string;
+  subheading?: string;
+  pill: string;
+}
+
+const TitleSection = ({ title, subheading, pill }: TitleSectionProps) => {
+  return (
+    <React.Fragment>
+      <section
+        className="flex
+    flex-col
+    gap-4
+    justify-center
+    items-center
+    md:items-center
+    "
+      >
+        <article
+          className="rounded-full
+        p-[1px]
+        text-sm
+        dark:bg-gradient-to-r
+        dark:from-brand/brand-primaryBlue
+        dark:to-brand/brand-primaryPurple"
+        >
+          <div
+            className="rounded-full
+            px-3
+            py-1
+            dark:bg-black"
+          >
+            {pill}
+          </div>
+        </article>
+        {subheading ? (
+          <>
+            <h2
+              className="text-left
+        text-3xl
+        sm:text-5xl
+        sm:max-w-[750px]
+        md:text-center
+        font-semibold
+        "
+            >
+              {title}
+            </h2>
+            <p>{subheading}</p>
+          </>
+        ) : (
+          <h1
+            className="text-left
+            text-4xl
+            sm:text-6xl
+            sm:max-w-[850px]
+            md:text-center
+            font-semibold
+            "
+          >
+            {title}
+          </h1>
+        )}
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default TitleSection;
