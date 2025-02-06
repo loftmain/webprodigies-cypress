b71a9a3a869ec5ce43bc7a30dbbc5966b16a090e
jinjaelee <jjlee009@newsystech.co.kr>
2025-02-06 15:21:44 +0900

    .

---
+++ src/components/settings/settings-form.tsx
@@ -0,0 +1,9 @@
+import { useToast } from "@/hooks/use-toast";
+import React from "react";
+
+const SettingsForm = () => {
+  const { toast } = useToast();
+  return <div>SettingForm</div>;
+};
+
+export default SettingsForm;
