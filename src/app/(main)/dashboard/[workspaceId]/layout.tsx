28e95812958207a3d54f1badcc88f4b2bc308cec
jinjae <leejinjae7@gmail.com>
2025-02-15 19:29:18 +0900

    implemented mobile sidebar

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,3 +1,4 @@
+import MobileSidebar from "@/components/sidebar/mobile-sidebar";
 import Sidebar from "@/components/sidebar/sidebar";
 import { Toaster } from "@/components/ui/toaster";
 import React from "react";
@@ -11,6 +12,12 @@ const Layout: React.FC<LayoutProps> = ({ children, params }) => {
   return (
     <main className="flex overflow-hidden h-screen w-screen">
       <Sidebar params={params} />
+      <MobileSidebar>
+        <Sidebar
+          params={params}
+          className="w-screen inline-block sm:hidden"
+        ></Sidebar>
+      </MobileSidebar>
       <div className="dark:border-Neutrals/neutrals-12/70 border-l-[1px] w-full relative overflow-scroll">
         {children}
       </div>
--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -22,7 +22,6 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
         s.join(fileId);
       });
       s.on("send-changes", (deltas, fileId) => {
-        console.log("CHANGE", fileId, deltas);
         s.to(fileId).emit("receive-changes", deltas, fileId);
       });
       s.on("send-cursor-move", (range, fileId, cursorId) => {
